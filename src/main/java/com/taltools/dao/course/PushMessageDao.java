44d2a74af67b47b94c3ab6203d2a6cd5eca93ad1
cuizhongyuan <cuizhongyuan@tal.com>
2021-08-24 18:55:39 +0800

    增加分页

--- src/main/java/com/taltools/dao/course/PushMessageDao.java
+++ src/main/java/com/taltools/dao/course/PushMessageDao.java
@@ -1,6 +1,6 @@
 package com.taltools.dao.course;
 
-import com.registtools.entity.PushMessageEntity;
+
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -8,5 +8,5 @@ import java.util.Map;
 
 @Repository
 public interface PushMessageDao {
-    List<PushMessageEntity> getCountClasses(Map<String,Object> map);
+
 }
--- src/main/java/com/taltools/dao/regist/RegistStudentDao.java
+++ src/main/java/com/taltools/dao/regist/RegistStudentDao.java
@@ -1,11 +1,11 @@
 package com.taltools.dao.regist;
 
-import com.registtools.entity.RegistStudentEntity;
+
 import org.springframework.stereotype.Repository;
 
 
 @SuppressWarnings("ALL")
 @Repository
 public interface RegistStudentDao {
-    RegistStudentEntity getStudentId(String studentd);
+
 }
